7f4d4849fd1394092aebd6433086c8ab8a10c4e9
huonghuynh <e.huonghuynh@gmail.com>
2024-04-25 14:14:31 +0700

    Create function getcomputerChoice() to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log(`HELLO!`)
+const winKey = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper",
+}
+const computerChoice = Object.keys(winKey)
+function getComputerChoice(){
+    let randomIndex = Math.floor(Math.random()*computerChoice.length);
+    return computerChoice[randomIndex]
+}
+console.log(getComputerChoice());
+
+function getHumanChoice(){
+
+}
+
+function playRound(){
+
+}
